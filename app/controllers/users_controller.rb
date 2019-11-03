04a864f3e80c6b91458103186faafd60f4e25ea5
Samuel Guo <47992471+guosamuel@users.noreply.github.com>
2019-11-03 16:17:04 -0500

    added a show page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,4 +5,9 @@ class UsersController < ApplicationController
     render json: { users: @users }
   end
 
+  def show
+    @user = User.find(params[:id])
+    render json: {user: @user}
+  end
+
 end
